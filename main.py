15582f30f77022a16015e920d7c165ac9b4d9bd2
astralevolution <120140754+astralevolution@users.noreply.github.com>
2023-09-30 20:12:42 +0000

    bug fix

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 import os
 import random
 import string
-from flask import Flask, render_template, request, send_from_directory, session
-import secrets
-app = Flask(__name__)
-app.secret_key = secrets.token_hex(16)
 
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './'
 app.config['MAX_FILE_SIZE'] = 10 * 1024 * 1024  # 10MB
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}  # Add more extensions if needed
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -18,48 +16,44 @@ def generate_random_filename():
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(16))
 
-def get_counter():
-    counter = session.get('counter', 0)
-    return counter
-
-def update_counter(counter):
-    session['counter'] = counter
-
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    if session.get('uploaded_file'):
-        return render_template('upload_success.html', file_url=session['uploaded_file'])
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return render_template('upload_error.html', error='No file part'), 400
 
-    if 'file' not in request.files:
-        return render_template('upload_error.html', error='No file part'), 400
+        file = request.files['file']
 
-    file = request.files['file']
+        if file.filename == '':
+            return render_template('upload_error.html', error='No selected file'), 400
 
-    if file.filename == '':
-        return render_template('upload_error.html', error='No selected file'), 400
+        if file:
+            if request.content_length > app.config['MAX_FILE_SIZE']:
+                return render_template('upload_error.html', error='File size exceeds 10MB'), 400
 
-    if file:
-        if request.content_length > app.config['MAX_FILE_SIZE']:
-            return render_template('upload_error.html', error='File size exceeds 10MB'), 400
+            if not allowed_file(file.filename):
+                return render_template('upload_error.html', error='Invalid file type'), 400
 
-        if not allowed_file(file.filename):
-            return render_template('upload_error.html', error='Invalid file type'), 400
 
-        random_filename = generate_random_filename()
-        file_extension = file.filename.rsplit('.', 1)[1].lower()
-        filename = f'{random_filename}.{file_extension}'
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
+            random_filename = generate_random_filename()
+            file_extension = file.filename.rsplit('.', 1)[1].lower()
+            filename = f'{random_filename}.{file_extension}'
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
 
-        file_url = f'https://cdn.astralevolution.pro/{filename}'
-        session['uploaded_file'] = file_url
-        return render_template('upload_success.html', file_url=file_url)
+            file_url = f'https://cdn.astralevolution.pro/{filename}'
+            return redirect(url_for('upload_success', file_url=file_url))
 
-    return render_template('upload_error.html', error='Invalid file'), 400
+    return render_template('upload.html')
+
+@app.route('/upload_success', methods=['GET'])
+def upload_success():
+    file_url = request.args.get('file_url')
+    return render_template('upload_success.html', file_url=file_url)
 
 @app.route('/<filename>', methods=['GET'])
 def serve_uploaded_file(filename):
